c4bc8da86c29f0f110b333afef7c2524cb5946b4
xiteral128 <ywqih1206979@163.com>
2025-12-20 06:41:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-// index.js
 export default {
   async fetch(request, env) {
-    // 这行代码是关键！env.ASSETS.fetch 会处理所有静态文件
-    return env.ASSETS.fetch(request);
+    return await env.ASSETS.fetch(request);
   }
 };
